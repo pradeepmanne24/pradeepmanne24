7a26c89e5d37e899e364f986c719ec994a3ffdec
mannepradeep <pradeepmanne24@gmail.com>
2021-07-18 17:56:37 +0530

    instal commit
    intial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button @click="getlist">Load</button>
+
+    <div v-for="sample in samplelist" :key="sample.col">
+      <h3>{{ sample.col1 }}  {{ sample.name }} </h3>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Home",
+  data() {
+    return {
+      samplelist: [],
+    };
+  },
+  methods: {
+    getlist() {
+      axios
+        .get("./Sample.json")
+        .then((response) => {
+          this.samplelist = response.data;
+          console.log(this.samplelist);
+        })
+        .catch();
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
